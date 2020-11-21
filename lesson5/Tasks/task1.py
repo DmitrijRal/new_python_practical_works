af7e7b486c8cd71044ff30ba0219ccba7b27e393
DmitrijRal <DmitrijRal@gmail.com>
2020-11-21 10:02:12 +0300

    added 5 tasks for lesson5

---
+++ lesson5/Tasks/task1.py
@@ -0,0 +1,12 @@
+# Создать программно файл в текстовом формате, записать в него построчно данные, вводимые
+# пользователем.Об окончании ввода данных свидетельствует пустая строка.
+from itertools import cycle
+
+out_f = open("task1.txt", "w", encoding="UTF-8")
+while True:
+    write = input("Enter text: ")
+    if write =="":
+        break
+    else:
+        out_f.write(f"{write}\n")
+out_f.close()
